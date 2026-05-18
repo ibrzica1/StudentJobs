b72e3d0e2188bb675b7c14e4b3abfdbe7774a980
ibrzica1 <igorbrzica1@gmail.com>
2026-05-18 09:04:11 +0200

    Create form for creating jobs

--- resources/views/jobCreate.blade.php
+++ resources/views/jobCreate.blade.php
@@ -5,6 +5,71 @@
 @endsection
 
 @section("content")
-<p>Hello</p>
+
+<div>
+    <p>Booking details</p>
+
+    <div>
+        <p>Title / Your booking</p>
+        <div>
+            <input type="text" placeholder="Job Title">
+        </div>
+    </div>
+
+    <div>
+        <p>Location</p>
+        <div>
+            <input type="text" placeholder="City">
+        </div>
+    </div>
+
+    <div>
+        <p>How many moving helpers?</p>
+        <div>
+            <input type="number" name="" id="">
+        </div>
+    </div>
+
+    <div>
+        <p>Helper wage per person</p>
+        <div>
+            <input type="number" name="" id="">
+        </div>
+    </div>
+
+    <div>
+        <p>Deployment date</p>
+        <div>
+            <input type="date" name="" id="">
+        </div>
+    </div>
+
+    <div>
+        <p>Deployment time</p>
+        <p>From</p>
+        <input type="time" name="" id="">
+        <p>To</p>
+        <input type="time">
+    </div>
+</div>
+
+<div>
+    <p>Job Details</p>
+
+    <div>
+        <p>Description</p>
+        <textarea name="" id="" cols="50" rows="5"></textarea>
+    </div>
+
+    <div>
+        <p>Tasks</p>
+        <textarea name="" id="" cols="50" rows="5"></textarea>
+    </div>
+
+    <div>
+        <p>Requirements</p>
+        <textarea name="" id="" cols="50" rows="5"></textarea>
+    </div>
+</div>
 
 @endsection
