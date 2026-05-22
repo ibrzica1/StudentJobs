9e47d5c4de119a6989b157669f28eac8a7d4c44a
ibrzica1 <igorbrzica1@gmail.com>
2026-05-22 10:41:43 +0200

    Create column type

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,17 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Job extends Model
 {
     protected $table = 'jobs';
 
     protected $fillable = [
-        'title','employer_id','location_id','address','setting_type',
+        'type','title','employer_id','location_id','address','setting_type',
         'weekly_hours','employee_amount','wage','start_date','from','to','duration',
-        'urgent','description','tasks','expectetion','offer'
+        'urgent','description','tasks','expectation','offer'
     ];
+
+    const JOB = "job";
+    const HELPER_JOB = "helper-job";
 
     public function employer(): BelongsTo
     {
