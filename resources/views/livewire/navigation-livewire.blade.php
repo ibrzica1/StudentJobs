3932c7010f55152decf83076ccc2d9d3b28f014f
ibrzica1 <igorbrzica1@gmail.com>
2026-06-04 09:12:15 +0200

    Add CSS to a navigation

--- resources/views/livewire/navigation-livewire.blade.php
+++ resources/views/livewire/navigation-livewire.blade.php
@@ -1,17 +1,21 @@
 <nav class="position-relative navbar navbar-light bg-light py-0 px-3 shadow-sm">
     <div class="container-fluid d-flex justify-content-between align-items-center">
-        <button class="btn p-0 border-0 bg-transparent"
+        <button class="btn px-3 py-3 border-0 menu-hover"
                 wire:click="$wire.toggleMenu">
             <img 
                 src="{{ asset('storage/images/icons/menu-bar.png') }}" 
                 alt="Menu"
-                style="width: 42px; height: 42px; object-fit: contain;"
+                style="width: 32px; height: 32px; object-fit: contain;"
             >
         </button>
-        <div class="position-absolute top-100 start-0" 
-        wire:show="showMenu">
-            <a href="" class="d-block p-2 text-decoration-none">Find a employee</a>
-            <a href="" class="d-block p-2 text-decoration-none">Find a helper</a>
+        <div class="position-absolute top-100 start-0 px-3 py-4" 
+        style="background-color: #f4bdbd; border-bottom-right-radius: 10px;"
+        wire:show="showMenu"
+        wire:click.outside="showMenu = false">
+            <a href="" class="d-block px-4 py-2 text-decoration-none rounded menu-link">
+                Find a employee</a>
+            <a href="" class="d-block px-4 py-2 text-decoration-none rounded menu-link">
+                Find a helper</a>
         </div>
         
         <a href="/" class="navbar-brand m-0 p-0">
@@ -22,22 +26,27 @@
             >
         </a>
 
-        <button class="btn p-0 border-0 bg-transparent"
+        <button class="btn px-3 py-3 border-0 home-hover"
                 wire:click="$wire.toggleProfile">
             <img 
                 src="{{ asset('storage/images/icons/home.png') }}" 
                 alt="Home"
-                style="width: 42px; height: 42px; object-fit: contain;"
+                style="width: 32px; height: 32px; object-fit: contain;"
             >
         </button>
 
-        <div class="position-absolute top-100 end-0 px-5 py-4"
-        style="background-color: #cef1ea; border-bottom-left-radius: 10px;"
-        wire:show="showProfile">
-            <a href="" class="d-block p-2 text-decoration-none bg-white">Profile & Account</a>
-            <a href="" class="d-block p-2 text-decoration-none">My Ads</a>
-            <a href="" class="d-block p-2 text-decoration-none">My Bills</a>
-            <a href="" class="d-block p-2 text-decoration-none">Logout</a>
+        <div class="position-absolute top-100 end-0 px-3 py-4"
+        style="background-color: #c4eccd; border-bottom-left-radius: 10px;"
+        wire:show="showProfile"
+        wire:click.outside="showProfile = false">
+            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
+                Profile & Account</a>
+            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
+                My Ads</a>
+            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
+                My Bills</a>
+            <a href="" class="d-block text-danger p-2 text-decoration-none px-4 py-2 rounded menu-link">
+                Logout</a>
         </div>
 
     </div>
