db3749f393e02dbf01cf49ccd384ef9284ec8195
ibrzica1 <igorbrzica1@gmail.com>
2026-06-07 06:36:59 +0200

    Create logout functionality in navigation

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,6 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-Alpine.start();
+//Alpine.start();
 
 
--- resources/views/livewire/navigation-livewire.blade.php
+++ resources/views/livewire/navigation-livewire.blade.php
@@ -1,53 +1,47 @@
-<nav class="position-relative navbar navbar-light bg-light py-0 px-3 shadow-sm">
-    <div class="container-fluid d-flex justify-content-between align-items-center">
-        <button class="btn px-3 py-3 border-0 menu-hover"
+<nav class="relative bg-white shadow-sm py-0 px-3">
+    <div class="flex justify-between items-center w-full">
+        
+        <button class="p-3 border-0 transition-colors hover:bg-[#f4bdbd] rounded"
                 wire:click="$wire.toggleMenu">
-            <img 
-                src="{{ asset('storage/images/icons/menu-bar.png') }}" 
-                alt="Menu"
-                style="width: 32px; height: 32px; object-fit: contain;"
-            >
+            <img src="{{ asset('storage/images/icons/menu-bar.png') }}" 
+                 alt="Menu"
+                 class="w-8 h-8 object-contain">
         </button>
-        <div class="position-absolute top-100 start-0 px-3 py-4" 
-        style="background-color: #f4bdbd; border-bottom-right-radius: 10px;"
-        wire:show="showMenu"
-        wire:click.outside="showMenu = false">
-            <a href="" class="d-block px-4 py-2 text-decoration-none rounded menu-link">
-                Find a employee</a>
-            <a href="" class="d-block px-4 py-2 text-decoration-none rounded menu-link">
-                Find a helper</a>
+
+        <div class="absolute top-full left-0 px-3 py-4 bg-[#f4bdbd] rounded-br-[10px] z-50" 
+             wire:show="showMenu"
+             wire:click.outside="showMenu = false">
+            <a href="#" class="block px-4 py-2 hover:bg-white/50 rounded transition-colors text-gray-800">Find an employee</a>
+            <a href="#" class="block px-4 py-2 hover:bg-white/50 rounded transition-colors text-gray-800">Find a helper</a>
         </div>
         
-        <a href="/" class="navbar-brand m-0 p-0">
-            <img 
-                src="{{ asset('storage/images/icons/PageLogo.png') }}" 
-                alt="Logo"
-                style="height: 85px; object-fit: contain;"
-            >
+        <a href="/" class="p-0 m-0">
+            <img src="{{ asset('storage/images/icons/PageLogo.png') }}" 
+                 alt="Logo"
+                 class="h-[85px] object-contain">
         </a>
 
-        <button class="btn px-3 py-3 border-0 home-hover"
+        <!-- Profile Button -->
+        <button class="p-3 border-0 transition-colors hover:bg-[#c4eccd] rounded"
                 wire:click="$wire.toggleProfile">
-            <img 
-                src="{{ asset('storage/images/icons/home.png') }}" 
-                alt="Home"
-                style="width: 32px; height: 32px; object-fit: contain;"
-            >
+            <img src="{{ asset('storage/images/icons/home.png') }}" 
+                 alt="Home"
+                 class="w-8 h-8 object-contain">
         </button>
 
-        <div class="position-absolute top-100 end-0 px-3 py-4"
-        style="background-color: #c4eccd; border-bottom-left-radius: 10px;"
-        wire:show="showProfile"
-        wire:click.outside="showProfile = false">
-            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
-                Profile & Account</a>
-            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
-                My Ads</a>
-            <a href="" class="d-block p-2 text-decoration-none px-4 py-2 rounded menu-link">
-                My Bills</a>
-            <a href="" class="d-block text-danger p-2 text-decoration-none px-4 py-2 rounded menu-link">
-                Logout</a>
+        <!-- Profile Dropdown -->
+        <div class="absolute top-full right-0 px-3 py-4 bg-[#c4eccd] rounded-bl-[10px] z-50"
+             wire:show="showProfile"
+             wire:click.outside="showProfile = false">
+            <a href="#" class="block p-2 px-4 hover:bg-white/50 rounded transition-colors text-gray-800">Profile & Account</a>
+            <a href="#" class="block p-2 px-4 hover:bg-white/50 rounded transition-colors text-gray-800">My Ads</a>
+            <a href="#" class="block p-2 px-4 hover:bg-white/50 rounded transition-colors text-gray-800">My Bills</a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button href="{{route('logout')}}" class="block p-2 px-4 hover:bg-white/50 rounded transition-colors text-red-600">
+                    Logout</button>
+            </form>
+            
         </div>
-
     </div>
 </nav>
