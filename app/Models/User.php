673badcd00e411677ce6e3bfd6e798a4a2120ea5
ibrzica1 <igorbrzica1@gmail.com>
2026-06-09 16:29:41 +0200

    Create Migration for add profile picture to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'firstName',
+        'lastName',
+        'location_id',
+        'telephone',
+        'role',
+        'profile_picture',
     ];
 
     /**
---
+++ database/migrations/2026_06_08_161747_add_attributes_to_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstName')->after('password');
+            $table->string('lastName')->after('firstName');
+            $table->unsignedBigInteger('location_id')->after('lastName');
+            $table->string('telephone')->after('location_id');
+            $table->string('role')->after('telephone');
+
+            $table->foreign('location_id')->references('id')->on('locations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('user_location_id_foreign');
+            $table->dropColumn(['firstName', 'lastName', 'location_id', 'telephone', 'role']);
+        });
+    }
+};
