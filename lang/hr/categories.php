0be8b264567cd7b7341a85036ba43872346fd2b7
ibrzica1 <igorbrzica1@gmail.com>
2026-08-17 10:09:02 +0200

    Localization for categories

---
+++ lang/hr/categories.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Job;
+
+return [
+'All' => 'Sve',
+Job::CONSTRUCTION_HELPER => 'Gradnja',
+Job::MOVING_HELPER => 'Selidba',
+Job::DECLUTTER_HELPER => 'Spremanje',
+Job::EVENT_HELPER => 'Dogadaji',
+Job::LOGISTICS_HELPER => 'Logistika',
+Job::OFFICE_HELPER => 'Ured',
+Job::TUTOR_HELPER => 'Poduka',
+Job::IT_HELPER => 'It',
+Job::BABYSITTER_HELPER => 'Dadilja',
+Job::DRIVER_HELPER => 'Vozac',
+'Filter' => 'Filtriraj',
+];
--- resources/views/livewire/select-category.blade.php
+++ resources/views/livewire/select-category.blade.php
@@ -4,16 +4,16 @@
 
 <div class="d-flex justify-content-end 
 align-items-center column-gap-3 mx-4 my-2">
-    <label for="">{{ __('Select category') }}</label>
+    <label for="">{{ __('homepage.Select category') }}</label>
     <select wire:model="category" 
         class="form-select w-25 border rounded">
-        <option value="all">All</option>
+        <option value="all">{{__("categories.All")}}</option>
         @foreach (Job::ALLOWED_HELPER_TYPES as $category)
-            <option value="{{$category}}">{{$category}}</option>
+            <option value="{{$category}}">{{__("categories.$category")}}</option>
         @endforeach
     </select>
     <button wire:click='filterByCategory()'
     class="btn btn-secondary">
-        Filter
+        {{ __('categories.Filter') }}
     </button>
 </div>
