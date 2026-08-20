050e0bd00102c394a3eeb13c2f75472a294d7be3
ibrzica1 <igorbrzica1@gmail.com>
2026-08-20 15:23:21 +0200

    Localization for duration types

---
+++ lang/hr/durationTypes.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\Job;
+
+return [
+    Job::INDEFINITE => 'Neodredeno',
+    Job::WEEKS => '1-4 tjedana',
+    Job::MONTH1 => '1 mjesec',
+    Job::MONTH2 => '2 mjeseca',
+    Job::MONTH3 => '3-5 mjeseci',
+    Job::MONTH6 => '6 mjeseci',
+    Job::YEAR1 => '1 godina',
+    Job::YEAR2 => '2 godine',
+    Job::YEAR3 => '3 godine',
+];
