1a840d66ee6151a48a2b84ded392e14acdf00bf8
ibrzica1 <igorbrzica1@gmail.com>
2026-09-06 16:45:40 +0200

    Test if Job can be updated

--- tests/Feature/Model/Jobs/JobUpdateTest.php
+++ tests/Feature/Model/Jobs/JobUpdateTest.php
@@ -31,3 +31,41 @@ it('job edit page for authorised user', function() {
     $response = $this->actingAs($employer)->get(route('job.edit',$job->id));
     $response->assertOk();
 });
+
+it('job edit page for unauthenticated user', function() {
+    
+    (new GermanLocationsSeeder())->run();
+    $employer = User::factory()->employer()->create();
+    $company = Company::factory(['user_id' => $employer->id])->create();
+    $job = Job::factory(['employer_id' => $employer->id, 'company_id' => $company->id])->create();
+    $response = $this->get(route('job.edit',$job->id));
+    $response->assertRedirect('/login');
+});
+
+it('job update has no errors', function() {
+
+    $this->withoutExceptionHandling();
+    (new GermanLocationsSeeder())->run();
+    $employer = User::factory()->employer()->create();
+    $company = Company::factory(['user_id' => $employer->id])->create();
+    $job = Job::factory(['employer_id' => $employer->id, 'company_id' => $company->id])->create();
+    $response = $this->actingAs($employer)
+                     ->patch('/job/update/' .$job->id,[
+                        'type' => Job::JOB,
+                        'category' => Job::IT_HELPER,
+                        'title' => 'TITLE',
+                        'employer_id' => $employer->id,
+                        'location_id' => 4,
+                        'company_id' => $company->id,
+                        'wage' => 20,
+                        'setting_type' => Job::ALLOWED_SETTING_TYPES[0],
+                        'weekly_hours' => 20,
+                        'start_date' => fake()->dateTimeBetween('now','+1 month')->format('Y-m-d'),
+                        'duration' => fake()->randomElement(Job::ALLOWED_DURATION_TYPES),
+                        'description' => 'DESCRIPTION',
+                        'expectation' => 'EXPECTATIONS',
+                        'offer' => 'OFFER',
+                     ]);
+    $response->assertSessionHasNoErrors()
+             ->assertRedirect('/job/my-ads');
+});
