8c1508604a65089946c92f742e494f33be5f3fbb
ibrzica1 <igorbrzica1@gmail.com>
2026-09-07 11:12:48 +0200

    Create Applicarion model

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Application extends Model
 {
-    
+    protected $table = 'applications';
+
+    protected $fillable = [
+        'text','user_id','job_id','status'
+    ];
+
+    const PENDING = "pending";
+    const APPROVED = "approved";
+    const REJECTED = "rejected";
+
+    const ALLOWED_STATUSES = [
+        self::PENDING, self::APPROVED, self::REJECTED
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(Job::class);
+    }
 }
