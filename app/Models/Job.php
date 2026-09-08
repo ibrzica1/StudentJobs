a6eceb3af76136557f3f6651506cfd7ed7a23e5f
ibrzica1 <igorbrzica1@gmail.com>
2026-09-08 12:32:23 +0200

    Define applications relationship in Job and User model

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
@@ -79,6 +80,11 @@ class Job extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
     }
 
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -74,4 +74,9 @@ class User extends Authenticatable
         return $this->hasMany(Company::class,'user_id','id');
     }
 
+    public function applications(): HasMany
+    {
+        return $this->hasMany(Application::class);
+    }
+
 }
