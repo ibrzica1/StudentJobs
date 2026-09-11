e8ba29027295f743c55c0b68f6077f2ebbdc7a81
ibrzica1 <igorbrzica1@gmail.com>
2026-09-11 13:56:37 +0200

    Localization for application

--- lang/en/applications.php
+++ lang/en/applications.php
@@ -10,4 +10,18 @@ return [
     'View Application' => 'View Application',
     'Accept' => 'Accept',
     'Reject' => 'Reject',
+
+    'from' => 'from',
+    'MORE INFORMATION' => 'MORE INFORMATION',
+    'None' => 'None',
+    'Car licence' => 'Car licence',
+    'Truck licence' => 'Truck licence',
+    'Car available' => 'Car available',
+    'YES' => 'YES',
+    'NO' => 'NO',
+    'Mobility' => 'Mobility',
+    'Applicants attachment' => 'Applicants attachment',
+    'CV' => 'CV',
+    'University' => 'University',
+    'Certificates' => 'Certificates',
 ];
