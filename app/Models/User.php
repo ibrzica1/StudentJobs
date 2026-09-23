7668268b6224ab3b4a1bb7a1f85a8e396c580b81
ibrzica1 <igorbrzica1@gmail.com>
2026-09-23 12:20:35 +0200

    Create ratings relathionship in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -96,4 +96,9 @@ class User extends Authenticatable
         return $this->hasMany(Application::class);
     }
 
+    public function ratings(): HasMany
+    {
+        return $this->hasMany(Rating::class);
+    }
+
 }
