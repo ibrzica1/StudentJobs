b9515fad21a53b7c5e5aa10e37874a7ace57d982
ibrzica1 <igorbrzica1@gmail.com>
2026-09-23 12:20:58 +0200

    Create Rating model

---
+++ app/Models/Rating.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ratings';
+
+    protected $fillable = [
+        'score','comment','user_id','job_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(Job::class);
+    }
+}
