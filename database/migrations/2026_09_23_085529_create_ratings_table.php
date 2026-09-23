bf3440231668f96d9e499f56df147f52601c37f9
ibrzica1 <igorbrzica1@gmail.com>
2026-09-23 12:21:23 +0200

    Migration create ratings table

---
+++ database/migrations/2026_09_23_085529_create_ratings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+ 
+    public function up(): void
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('score');
+            $table->text('comment')->nullable();
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->cascadeOnDelete();
+            $table->foreignId('job_id')
+            ->constrained('jobs');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
